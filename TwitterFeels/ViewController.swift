94db552d08c7d3ab00e402b7fec863c968a443bb
Shawn Chandwani <ShawnC7208@gmail.com>
2020-07-28 12:28:09 -0500

    Add git ignore with twitter api

--- TwitterFeels/ViewController.swift
+++ TwitterFeels/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Swifter
 
 class ViewController: UIViewController {
 
@@ -16,11 +17,25 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        // Instantiation using Twitter's OAuth Consumer Key and secret
+        let TWITTER_CONSUMER_KEY = valueForAPIKey(named: "TWITTER_CONSUMER_KEY")
+        let TWITTER_CONSUMER_SECRET = valueForAPIKey(named: "TWITTER_CONSUMER_SECRET")
+        let swifter = Swifter(consumerKey: TWITTER_CONSUMER_KEY, consumerSecret: TWITTER_CONSUMER_SECRET)
+        
     }
 
 
     @IBAction func predictPressed(_ sender: UIButton) {
         
+    }
+    
+    func valueForAPIKey(named keyname:String) -> String {
+      // Credit to the original source for this technique at
+      // http://blog.lazerwalker.com/blog/2014/05/14/handling-private-api-keys-in-open-source-ios-apps
+        let filePath = Bundle.main.path(forResource: "Secrets", ofType: "plist")
+      let plist = NSDictionary(contentsOfFile:filePath!)
+      let value = plist?.object(forKey: keyname) as! String
+      return value
     }
 }
 
